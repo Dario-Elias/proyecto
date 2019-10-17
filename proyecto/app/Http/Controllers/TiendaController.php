a6fa67244d16d2c1d0b4cdb0b1b252f87abe6f2d
Dario-Elias <daeliasc@gmail.com>
2019-10-17 16:02:22 -0600

    Menu Tienda Creada

--- proyecto/app/Http/Controllers/TiendaController.php
+++ proyecto/app/Http/Controllers/TiendaController.php
@@ -3,17 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\tienda;
 
-class TiendaControl extends Controller
+class TiendaController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index()
     {
-        //
+        return view ("tiendas.index");
     }
 
     /**
